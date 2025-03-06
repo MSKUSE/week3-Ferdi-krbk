2717918b95eecd59e02eb1c96c71be53aa0ff810
Ferdi-krbk <ferdiderebasi@posta.mu.edu.tr>
2025-03-06 13:06:37 +0300

    Week 3 work is done.

--- week3/src/FindMinimum.java
+++ week3/src/FindMinimum.java
@@ -13,7 +13,7 @@ public class FindMinimum {
         //boolean min_num2 = num2 < num1 && num2 < num3;
         //int minimum = min_num1 ? num1:
         //                        min_num2 ? num2:num3;
-
+        //System.out.println(minimum);
 
         if (num1 < num2 && num1 < num3) {
             System.out.println("The smallest number is num1: " + num1);
